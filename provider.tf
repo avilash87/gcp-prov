683646c1c2be084868f32d8bd0b6a5bdeef1d8fb
avilash87 <avilash.jena@gmail.com>
2024-08-09 19:38:34 +0100

    updated terrform version

--- provider.tf
+++ provider.tf
@@ -8,6 +8,15 @@ terraform {
   }
 }
 
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.48.0"
+    }
+  }
+}
+
 provider "google" {
   project = var.gcp_project_id
   region  = var.gcp_region
