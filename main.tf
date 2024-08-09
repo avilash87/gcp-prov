c00fe1705e34882a643e77c7de7fddee65dd7a8d
avilash87 <avilash.jena@gmail.com>
2024-08-09 19:19:10 +0100

    create storage bucket updated

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
 resource "google_storage_bucket" "auto-expire" {
   name          = "no-public-access-bucket"
-  location      = "${var.gcp_region}"
+  location      = var.gcp_region
   force_destroy = true
 
   public_access_prevention = "enforced"
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+
+    condition {
+      age = 30  # Objects older than 30 days will be deleted
+    }
+  }
 }
