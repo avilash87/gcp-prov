e694b3a48a078accedb30bf3bcc68f96e32823ff
avilash87 <avilash.jena@gmail.com>
2024-08-13 00:46:28 +0100

    Update main.tf removed bucket (#18)

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
- resource "google_storage_bucket" "auto-expire" {
-   name          = "no-public-access-bucket1-${var.gcp_project_id}"
-   location      = var.gcp_region
-   force_destroy = true
+# resource "google_storage_bucket" "auto-expire" {
+#   name          = "no-public-access-bucket1-${var.gcp_project_id}"
+#   location      = var.gcp_region
+#   force_destroy = true
 
-   public_access_prevention = "enforced"
+#   public_access_prevention = "enforced"
 
-   lifecycle_rule {
-     action {
-       type = "Delete"
-     }
+#   lifecycle_rule {
+#    action {
+#       type = "Delete"
+#     }
 
-     condition {
-       age = 30  # Objects older than 30 days will be deleted
-     }
-   }
- }
+#     condition {
+#       age = 30  # Objects older than 30 days will be deleted
+#     }
+#   }
+# }
