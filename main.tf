1bc76b387dee410105c51d72f215a01709cfd05f
jagdishwala-kapil <122120306+jagdishwala-kapil@users.noreply.github.com>
2024-08-12 16:35:48 +0100

    feat: Update main.tf (#4)

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "auto-expire" {
-  name          = "no-public-access-bucket-${var.gcp_project_id}-${var.bucket_suffix}"
+  name          = "no-public-access-bucket1-${var.gcp_project_id}-${var.bucket_suffix}"
   location      = var.gcp_region
   force_destroy = true
 
